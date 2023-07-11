a5c0530f3ba1866fc7522706e900791a3461ee08
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-07-11 18:25:48 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -6,5 +6,8 @@ const orderData=require('../controller/payementcontrol');
 router.get('/products',productData.allProducts)
 router.post('/payment',orderData.orders)
 router.post('/verify',orderData.verify)
+router.get('/products/:id',productData.cartProducts)
+router.post('/calender/:id',productData.calender)
+
 
 module.exports=router;
